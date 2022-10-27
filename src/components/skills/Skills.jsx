9ffba7c57586e6b04407a40e5a20addd26b9586b
mrchurches <laureanoiglesias34@gmail.com>
2022-10-27 01:34:06 -0300

    more responsive fixes (footer column)

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -18,97 +18,9 @@ import redux from "../../assets/img/redux.svg";
 import sequelize from "../../assets/img/sequelize.png";
 
 const Skills = () => {
-  // const responsive = {
-  //     superLargeDesktop: {
-  //       // the naming can be any, depends on you.
-  //       breakpoint: { max: 4000, min: 3000 },
-  //       items: 5
-  //     },
-  //     desktop: {
-  //       breakpoint: { max: 3000, min: 1024 },
-  //       items: 3
-  //     },
-  //     tablet: {
-  //       breakpoint: { max: 1024, min: 464 },
-  //       items: 2
-  //     },
-  //     mobile: {
-  //       breakpoint: { max: 464, min: 0 },
-  //       items: 1
-  //     }
-  //   };
-  return (
-    // <section className='skill' id="skills">
-    //     <Container>
-    //         <Row>
-    //             <Col>
-    //             <div className='skill-bx'>
-    //                 <h2>Soft Skills</h2>
-    //                 <p>My experience outside the IT world is +6 years in customer service, both face-to-face and by telephone, which allows me to have excellent communication, oral and written, and a very good active listening.<br/>
-    //                 In addition, in my last project i work in teams of seven teammates and put on practice and evidence my facility to communicate me, share my ideas, and organization to achieve our objectives.
-    //                 </p>
-    //                 <h2>Tech Skills</h2>
 
-    //                 <Carousel responsive={responsive} infinite={true} className="skill-slider">
-    //                     <div class="img-skill">
-    //                         <img src={js} alt="img-skill"/>
-    //                         <h5>Javascript</h5>
-    //                     </div>
-    //                     <div class="img-skill">
-    //                         <img src={nodejs} alt="img-skill"/>
-    //                         <h5>NodeJS</h5>
-    //                     </div>
-    //                     <div class="img-skill">
-    //                         <img src={ts} alt="img-skill"/>
-    //                         <h5>Typescript</h5>
-    //                     </div>
-    //                     <div class="img-skill">
-    //                         <img src={html} alt="img-skill"/>
-    //                         <h5>HTML5</h5>
-    //                     </div>
-    //                     <div class="img-skill">
-    //                         <img src={css} alt="img-skill"/>
-    //                         <h5>CSS3</h5>
-    //                     </div>
-    //                     <div class="img-skill">
-    //                         <img src={react} alt="img-skill"/>
-    //                         <h5>ReactJS</h5>
-    //                     </div>
-    //                     <div class="img-skill">
-    //                         <img src={redux} alt="img-skill"/>
-    //                         <h5>Redux</h5>
-    //                     </div>
-    //                     <div class="img-skill">
-    //                         <img src={express} alt="img-skill"/>
-    //                         <h5>Express</h5>
-    //                     </div>
-    //                     <div class="img-skill">
-    //                         <img src={git} alt="img-skill"/>
-    //                         <h5>Git</h5>
-    //                     </div>
-    //                     <div class="img-skill">
-    //                         <img src={postgres} alt="img-skill"/>
-    //                         <h5>Postgres/PostgreSQL</h5>
-    //                     </div>
-    //                     <div class="img-skill">
-    //                         <img src={sequelize} alt="img-skill"/>
-    //                         <h5>Sequelize</h5>
-    //                     </div>
-    //                     <div class="img-skill">
-    //                         <img src={rb} alt="img-skill"/>
-    //                         <h5>React-Bootstrap</h5>
-    //                     </div>
-    //                     <div class="img-skill">
-    //                         <img src={bootstrap} alt="img-skill"/>
-    //                         <h5>Bootstrap</h5>
-    //                     </div>
-    //                 </Carousel>
-    //             </div>
-    //             </Col>
-    //         </Row>
-    //     </Container>
-    //     {/* <img className='background-image-left' src={colorSharp} alt="sharp"/> */}
-    // </section>
+  return (
+ 
     <section class="triangle">
       <div
         class="d-flex flex-column align-items-center justify-content-center w-100"
