e33e0d2e0f4908ded8c40b1cd2a6b132e328c28e
mrchurches <laureanoiglesias34@gmail.com>
2022-11-03 14:45:07 -0300

    fix center in skills

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,35 +1,34 @@
 import React from "react";
 import { Col, Container, Row, Tab } from "react-bootstrap";
 import ProjectCard from "./ProjectCard.jsx";
-import colorSharp2 from "../../assets/img/color-sharp.png";
-import portfolio from "../../assets/img/portfolio.png";
+import pi from "../../assets/img/pi.gif";
+import portfolio from "../../assets/img/portfolio.gif";
+import pf from "../../assets/img/pf.gif";
 
 const Projects = () => {
   const projects = [
     {
-      title: "The countries page",
+      title: "World",
       description:
         "A SPA(single page aplication) that uses React, Redux in front and express, nodeJS, Postgres and Sequelize in the back",
-      imgUrl:
-        "https://media0.giphy.com/media/pZ9usVhLXcAIsLYRoy/giphy.gif?cid=790b761112280b352e7818f69f48ee44d23d48c2311bd64d&rid=giphy.gif&ct=g",
-        repository: "https://github.com/mrchurches/PI-Countries-main",
-        deploy:"https://countries-page-plum.vercel.app/"
+      imgUrl: pi,
+      repository: "https://github.com/mrchurches/PI-Countries-main",
+      deploy: "https://pi-world.vercel.app/home",
     },
     {
       title: "Videogames e-commerce",
       description: "a fully e-commerce page",
-      imgUrl:
-        "https://media3.giphy.com/media/PhRugtKQ960Htna4MN/giphy.gif?cid=790b7611afa889cd816110b49daaffe0f0a3f628c5dc3410&rid=giphy.gif&ct=g",
-        repository:"https://github.com/mrchurches/e-commerce_client",
-        deploy:"https://e-commerce-videogames.vercel.app/"
+      imgUrl: pf,
+      repository: "https://github.com/mrchurches/e-commerce_client",
+      deploy: "https://e-commerce-videogames.vercel.app/",
     },
     {
       title: "Portfolio",
       description:
         "This portfolio do it with React-Bootstrap, NodeMailer, Animate.css, Express",
       imgUrl: portfolio,
-      repository:"https://github.com/mrchurches/Portfolio",
-      deploy:"https://portfolio-laureano.vercel.app/"
+      repository: "https://github.com/mrchurches/Portfolio",
+      deploy: "https://portfolio-laureano.vercel.app/",
     },
   ];
   return (
@@ -44,7 +43,7 @@ const Projects = () => {
           })}
         </div>
       </div>
-    <section class="curved"></section>
+      <section class="curved"></section>
     </div>
   );
 };
--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -1,18 +1,15 @@
 import React from "react";
 import "react-multi-carousel/lib/styles.css";
 
-
 const Skills = () => {
-
   return (
- 
     <section class=" d-flex triangle w-50 align-items-center justify-content-center w-100">
       <div
         class="d-flex flex-column align-items-center justify-content-center"
         id="skills"
       >
         <div class="skills">
-          <div class="">
+          <div>
             <h2 class="hvr-underline-from-center">Tech skills</h2>
           </div>
           <div class="d-flex flex-wrap align-items-center justify-content-center skill-box">
@@ -38,16 +35,15 @@ const Skills = () => {
           </div>
           <div class="flex-wrap text-center skill-box">
             <p>
-              My experience outside the IT world is +6 years in customer service,
-              both face-to-face and by telephone, which allows me to have
-              excellent communication, oral and written, and a very good active
-              listening. In addition, in my last project i work in teams of seven
-              teammates and put on practice and evidence my facility to
+              My experience outside the IT world is +6 years in customer
+              service, both face-to-face and by telephone, which allows me to
+              have excellent communication, oral and written, and a very good
+              active listening. In addition, in my last project i work in teams
+              of seven teammates and put on practice and evidence my facility to
               communicate me, share my ideas, and organization to achieve our
               objectives.
             </p>
           </div>
-
         </div>
       </div>
     </section>
